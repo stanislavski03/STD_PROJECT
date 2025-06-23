f51520068417c5a19dce08d27a6a8ebe50cde9ee
stanislavski <cookie050329@gmail.com>
2025-06-23 14:49:48 +0300

    Начало разработки

---
+++ src/js/main.js
@@ -0,0 +1,54 @@
+import '../scss/main.scss';
+import Swiper from 'swiper';
+import 'swiper/css';
+import './map';
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+//КНОПКА ПРОКРУТКИ
+
+  document.getElementById('back-to-top').addEventListener('click', function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Плавная прокрутка
+    });
+  });
+
+  // Показываем/скрываем кнопку при прокрутке
+  window.addEventListener('scroll', function () {
+    const backToTopButton = document.getElementById('back-to-top');
+    if (window.pageYOffset > 300) {
+      backToTopButton.style.display = 'flex';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+
+  // Инициализация - скрываем кнопку при загрузке, если мы уже вверху
+  document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.getElementById('back-to-top');
+    backToTopButton.style.display = window.pageYOffset > 300 ? 'flex' : 'none';
+  });
+
+
+
+  
+
+
+
+
+})
+
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { watch } = require('fs');
+
+const pages = fs.readdirSync(path.resolve(__dirname, 'src/pages')).filter(file => file.endsWith('.html'));
+
+const otherPages = pages.filter(page => page !== 'index.html');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][hash][ext][query]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/pages/index.html',
+    filename: 'index.html',
+  }),
+
+  // Остальные страницы
+  ...otherPages.map(page => new HtmlWebpackPlugin({
+    template: `./src/pages/${page}`,
+    filename: page,
+  })),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/img',
+          to: 'img',
+          noErrorOnMissing: true,
+          globOptions: {
+            ignore: ['**/.DS_Store']
+          }
+        }
+      ]
+    })
+
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      watch: true,
+    },
+    hot: true,
+    open: true,
+    port: 8080,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false
+      }
+    }
+  }
+};
